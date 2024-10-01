465c4f0b5f267436ec890792a07b13065b3a0146
GalaxionZero <zetayu24@gmail.com>
2024-10-01 13:13:44 +0800

    posttest 4 files; testing server hosting

---
+++ POSTTEST4/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kivotos State Broadcast</title>
+
+    <link rel="Icon" href="assets/Prime_Student_Council_Icon.webp">
+    <link rel="stylesheet" href="styles/home.css">
+</head>
+<body>
+    <nav class="navbar-section">
+        <img src="assets/Prime_Student_Council_Icon.webp" alt="GSC Icon" width="100" height="100">
+        
+        <menu id="navbar-list">
+            <li class="navbar-item">
+                <a href=#home>
+                    Home
+                </a>
+            </li>
+            <li class="navbar-item">
+                <a href=#about-us>
+                    About Us
+                </a>
+            </li>
+            <li class="navbar-item">
+                <a href="#" id="news">
+                    News
+                </a>
+            </li>
+        </menu>
+        
+        <button id="dark-mode-toggle" class="dark-mode-toggle">Change OS</button>
+        
+        <button class="hamburger" id="hamburger">
+            <img src="assets/Blue_Archive_EN_logo.png" alt="hamburger" width="100">
+        </button>
+        
+    </nav>
+    
+    <main class="hero-section" id="home">
+        <div class="hero-image"></div>
+        <div class="hero-container">
+            <hgroup>
+                <h1 class="hero-title">
+                    Kivotos State Broadcast
+                </h1>
+                
+                <p class="hero-description">
+                    The official broadcasting platform of the General Student Council
+                </p>
+                <button id="audio-toggle" class="hero-description">Immerse Yourself</button>
+            </hgroup>
+        </div>
+        
+    </main>
+
+    <section class="card-section">
+        <div class="container">
+            <h1>Meet Our Academies</h1>
+        </div>
+    </section>
+
+    <section class="card-section1">
+        <div class="card">
+            <div class="container-card" style="background-image: url(assets/Abydos_Icon.webp); background-repeat: no-repeat; background-position-x: center; background-position-y: top;" >
+                <h4><b>Abydos High School</b></h4>
+                <p>It used to be a thriving and prestigious school before desertification. Now, all that remains is a shadow of its former glory, with nothing more than an annex building and five students with an enormous debt to pay off.</p>
+            </div>
+        </div>
+        
+        <div class="card">
+            <div class="container-card" style="background-image: url(assets/Gehenna_Icon.webp); background-repeat: no-repeat; background-position-x: center; background-position-y: top;">
+                <h4><b>Gehenna Academy</b></h4>
+                <p>A school that runs along the concept of "Freedom and Chaos." In contrast to its long rival, Trinity, this school is arguably the most chaotic out of all the schools.</p>
+            </div>
+        </div>
+        
+        <div class="card">
+            <div class="container-card" style="background-image: url(assets/Millennium_Icon.webp); background-repeat: no-repeat; background-position-x: center; background-position-y: top;">
+                <h4><b>Millennium Science School</b></h4>
+                <p>Despite its young age, it has carved its own path to become one of the largest schools in Kivotos. It values logic and technical skill over everything. If you're looking for technology, this is the place to go.</p>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="container-card" style="background-image: url(assets/Trinity_Icon.webp); background-repeat: no-repeat; background-position-x: center; background-position-y: top;">
+                <h4><b>Trinity General School</b></h4>
+                <p>A seemingly religious school that values order and etiquette. Unlike its long rival, Gehenna, this school is considerably the most orderly out of any school in Kivotos...at least, on the surface.</p>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="container-card" style="background-image: url(assets/Hyakkiyako_Icon.webp); background-repeat: no-repeat; background-position-x: center; background-position-y: top;">
+                <h4><b>Hyakkiyako Alliance Academy</b></h4>
+                <p>An academy made up of a coalition of numerous different clubs, each with their own cultures and rules. The academy frequents in large-scale activities such as festivals.</p>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="container-card" style="background-image: url(assets/Shanhaijing_Icon.webp); background-repeat: no-repeat; background-position-x: center; background-position-y: top;">
+                <h4><b>Shanhaijing Senior Secondary School</b></h4>
+                <p>A school that focuses more on business and commerce, visibly prioritizing money over culture. This school is more known for its delicious restaurants and tourism.</p>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="container-card" style="background-image: url(assets/Red_Winter_Icon.webp); background-repeat: no-repeat; background-position-x: center; background-position-y: top;">
+                <h4><b>Red Winter Federal Academy </b></h4>
+                <p>A snow-covered academy with an emphasis on communal effort...when it's not busy staging revolutions against its student council, that is. With its large territory, getting lost is pretty easy.</p>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="container-card" style="background-image: url(assets/Valkyrie_Icon.webp); background-repeat: no-repeat; background-position-x: center; background-position-y: top;">
+                <h4><b>Valkyrie Police Academy </b></h4>
+                <p>A school responsible for maintaining order and peace all over Kivotos. Despite being the main law enforcement agency in Kivotos, they have low reaction times and a lack of important security protocols.</p>
+            </div>
+        </div>
+
+    </section>
+
+    <footer>
+        <div class="footer-container" id="about-us">
+            <div class="footer-content">
+                <h1 class="footer-title">About Us</h1>
+                <p class="footer-description">Nama: Mohammad Guntur Adyatma</p>   
+                <p class="footer-description">NIM: 2309106023</p>
+                <p>Copyright of Nexon Co., Ltd.</p>
+            </div>
+        </div>
+    </footer>
+
+    <audio id="background-audio" loop>
+        <source src="assets/Aoharu.mp3" type="audio/mp3" >
+    </audio>
+    
+    <script src="scripts/scripts.js"></script>
+</body>
+
+</html>
